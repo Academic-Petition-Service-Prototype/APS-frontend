637e6b6c567152a2f80da3852dff851a7e840ec8
Chayanin Buasala <chayanin.gamer@gmail.com>
2022-01-29 00:19:55 +0700

    UPDATECODE UI ALL  Secretary

---
+++ src/views/Secretary/Tracking.vue
@@ -0,0 +1,148 @@
+
+<template>
+  <div id="Trackingofficer" class="bg-color">
+    <NavbarSecretary />
+    <v-card class="cardmargin">
+      <h2 class="text-center pa-5">
+        สถานะคำร้อง
+        <v-divider></v-divider>
+      </h2>
+
+      <!-- วนเเสดงรายการสถาณะเอสาร -->
+
+      <v-expansion-panels>
+        <v-expansion-panel
+          v-for="listtracking in listtracking"
+          :key="listtracking.num"
+        >
+          <v-expansion-panel-header>
+            {{ listtracking.title }}
+            <!-- เเสดงชื่อเอกสาร -->
+            <v-spacer></v-spacer>
+
+            <!-- เเสดงขั้นนตอน-->
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <!-- เเสดงเนื้อหาข้างใน -->
+
+            <v-container id="inspire">
+              <v-stepper alt-labels v-model="listtracking.checktracking">
+                <v-stepper-header>
+                  <v-stepper-step 
+                  :complete="listtracking.checktracking > 1"
+                  step="1"
+                  color="green"
+                   > ส่งยื่นคำร้อง </v-stepper-step>
+
+                  <v-divider></v-divider>
+
+                  <v-stepper-step
+                  :complete="listtracking.checktracking > 2"
+                  step="2" 
+                  color="green"
+                  > รับคำร้องเข้าระบบ </v-stepper-step>
+
+                  <v-divider></v-divider>
+
+                  <v-stepper-step 
+                  :complete="listtracking.checktracking > 3"
+                  step="3"
+                  color="green">
+                    การอนุมัติคำร้อง
+                  </v-stepper-step>
+
+                  <v-divider></v-divider>
+
+                  <v-stepper-step 
+                  :complete="listtracking.checktracking > 4"
+                  step="4"
+                  color="green"> ยื่นคำร้องสำเร็จ </v-stepper-step>
+
+                  
+                </v-stepper-header>
+                <v-stepper-items>
+                  <v-stepper-content step="1">
+                    <v-card class="mb-12" color="grey lighten-1" height="200px">
+                      <h2 class="cardshow">รายละเอียด</h2>
+                    </v-card>
+                  </v-stepper-content>
+
+                  <v-stepper-content step="2">
+                    <v-card class="mb-12" color="grey lighten-1" height="200px">
+                      <h2 class="cardshow">รายละเอียด</h2>
+                    </v-card>
+                  </v-stepper-content>
+
+                  <v-stepper-content step="3">
+                    <v-card class="mb-12" color="grey lighten-1" height="200px">
+                      <h2 class="cardshow">รายละเอียด</h2>
+                    </v-card>
+                  </v-stepper-content>
+
+
+                  <v-stepper-content step="4">
+                    <v-card class="mb-12" color="grey lighten-1" height="200px">
+                      <h2 class="cardshow">รายละเอียด</h2>
+                    </v-card>
+                  </v-stepper-content>
+
+
+                </v-stepper-items>
+              </v-stepper>
+            </v-container>
+
+            <!-- เเสดงเนื้อหาข้างใน -->
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import NavbarSecretary from "../../components/NavbarSecretary.vue";
+export default {
+  name: "TrackingSecretary",
+  components: {
+    NavbarSecretary,
+  },
+  data() {
+    return {
+      listtracking: [
+        {
+          num: 1,
+          title: "เอกสารที่1",
+          pointracking: 6,
+          checktracking: 3,
+          e1: 1,
+        },
+        {
+          num: 2,
+          title: "เอกสารที่2",
+          pointracking: 2,
+          checktracking: 3,
+          e1: 1,
+        },
+        {
+          num: 3,
+          title: "เอกสารที่2",
+          pointracking: 2,
+          checktracking: 1,
+          e1: 1,
+        },
+      ],
+      
+    };
+  },
+};
+</script>
+
+<style scoped>
+.bg-color {
+  background: #f0f0f0;
+  height: 100%;
+}
+.cardmargin {
+  margin: 2%;
+}
+</style>
---
+++ src/views/Secretary/ViewApproval.vue
@@ -0,0 +1,104 @@
+<template>
+  <div id="ChiefReport">
+    <NavbarSecretary />
+    <v-card class="cardmargin">
+      <h1 class="text-center p-4">
+        การอนุมัติ
+        <v-divider></v-divider>
+      </h1>
+      <v-row>
+        <v-col align="center" v-if="reports.length">
+          <v-btn width="150" height="50">
+            <v-icon>mdi-account</v-icon>
+            <div class="title">{{ reports.length }}</div>
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-card-title>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        >
+        </v-text-field>
+      </v-card-title>
+      <v-data-table :headers="headers" :items="reports" :search="search">
+        <template v-slot:[`item.actions`]="{ item }">
+          <v-btn small class="mr-2" @click="chageState(item.id)" disabled>
+            {{ item.report_state }}
+          </v-btn>
+        </template>
+      </v-data-table>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import NavbarSecretary from "../../components/NavbarSecretary.vue";
+import axios from "axios";
+export default {
+  name: "Approval",
+  components: {
+    NavbarSecretary,
+  },
+  data() {
+    return {
+      search: "",
+      headers: [
+        {
+          text: "ลำดับ",
+          align: "start",
+          value: "id",
+          width: 100,
+        },
+        { text: "รายการ", width: 500, value: "report_title" },
+
+        { text: "สถานะ", width: 200, value: "actions" },
+      ],
+      reports: [],
+    };
+  },
+  mounted() {
+    this.getreport();
+  },
+
+  methods: {
+    getreport() {
+      axios
+        .get(process.env.VUE_APP_URL + "reports")
+        .then((response) => {
+          // handle success
+          this.reports = response.data;
+        })
+        .catch((error) => {
+          // handle error
+          console.log(error);
+        });
+    },
+    chageState(id) {
+      console.log(id);
+      axios
+        .put(process.env.VUE_APP_URL + "reports", {
+          id: id,
+        })
+        .then(() => {
+          // handle success
+          this.$router.push("/Approvaldetaill/" + id);
+        })
+        .catch((error) => {
+          // handle error
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.cardmargin {
+  margin: 2%;
+}
+</style>
+
